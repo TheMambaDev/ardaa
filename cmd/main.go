f4fd2ea71d9ba4ff5eba99385c09f9f96cf26290
Mamba Mahamed <mamba@Abdusalams-MacBook-Air.local>
2023-09-17 12:14:58 +0300

    commit-kii ugu horeyay

---
+++ cmd/main.go
@@ -0,0 +1,5 @@
+package main
+
+/*
+*  This folder is for future cli integrations, so articles can be read from the terminal too
+ */
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/TheMambaDev/ardaa
+
+go 1.21.0
